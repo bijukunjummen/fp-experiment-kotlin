c06f7f8ae1b6be8de4ced41add7d7ae45a06421a
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-10-05 19:50:43 -0500

    polish code

--- src/main/kotlin/sample/adt/List.kt
+++ src/main/kotlin/sample/adt/List.kt
@@ -13,7 +13,7 @@ sealed class List<out A> {
     abstract val head: A
 
     abstract val tail: List<A>
-    
+
     fun <B> map(f: (A) -> B): List<B> {
         return when (this) {
             is Cons -> Cons(f(head), tail.map(f))
@@ -21,47 +21,67 @@ sealed class List<out A> {
         }
     }
 
+    fun drop(n: Int): List<A> {
+        return if (n <= 0)
+            this
+        else when (this) {
+            is Cons -> tail.drop(n - 1)
+            is Nil -> Nil
+        }
+    }
+
+    fun dropWhile(p: (A) -> Boolean): List<A> {
+        return when (this) {
+            is Cons -> if (p(this.head)) this.tail.dropWhile(p) else this
+            is Nil -> Nil
+        }
+    }
+
     fun <B> foldLeft(z: B, f: (B, A) -> B): B {
         tailrec fun foldLeft(l: List<A>, z: B, f: (B, A) -> B): B {
-            return when(l) {
+            return when (l) {
                 is Nil -> z
                 is Cons -> foldLeft(l.tail, f(z, l.head), f)
             }
         }
-        
+
         return foldLeft(this, z, f)
+    }
+
+    fun <B> foldRight(z: B, f: (A, B) -> B): B {
+        return when (this) {
+            is Cons -> f(this.head, tail.foldRight(z, f))
+            is Nil -> z
+        }
     }
 
     fun reverse(): List<A> {
         return foldLeft(Nil as List<A>, { b, a -> Cons(a, b) })
     }
 
-    fun <B> foldRight(z: B, f: (A, B) -> B): B {
+    fun <B> foldRightViaFoldLeft(z: B, f: (A, B) -> B): B {
         return reverse().foldLeft(z, { b, a -> f(a, b) })
     }
 
-    fun size(): Int = foldRight(0, {_, r -> r + 1})
+    fun size(): Int = foldRight(0, { _, r -> r + 1 })
 
-    fun append(l: List<@UnsafeVariance A>): List<A> {
+    fun <B : @UnsafeVariance A> append(l: List<@UnsafeVariance B>): List<A> {
         return when (this) {
             is Cons -> Cons(head, tail.append(l))
             is Nil -> l
         }
     }
-    
- 
-    
-    fun flatMap(f: (a: A) -> List<@UnsafeVariance A>): List<A> {
+
+    fun <B> flatMap(f: (a: A) -> List<@UnsafeVariance B>): List<B> {
         return flatten(map { a -> f(a) })
     }
-    
+
     companion object {
         fun <A> flatten(l: List<List<A>>): List<A> {
-            return l.foldRight(Nil as List<A>, { a, b -> a.append(b)})
+            return l.foldRight(Nil as List<A>, { a, b -> a.append(b) })
         }
     }
 }
-
 
 
 data class Cons<out T>(override val head: T, override val tail: List<T>) : List<T>()
--- src/test/kotlin/sample/adt/ListTest.kt
+++ src/test/kotlin/sample/adt/ListTest.kt
@@ -5,64 +5,72 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class ListTest {
-    
+
     @Test
     fun creatingAListRaw() {
-        val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+        val l1: List<Int> = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
 
-        assertThat(l.head).isEqualTo(1)
-        assertThat(l.tail).isEqualTo(Cons(2, Cons(3, Cons(4, Nil))))
-        assertThat(l.tail.head).isEqualTo(2)
+        assertThat(l1.head).isEqualTo(1)
+        assertThat(l1.tail).isEqualTo(Cons(2, Cons(3, Cons(4, Nil))))
+
+        val l2: List<String> = Nil
+        assertThat(l2).isEqualTo(Nil)
     }
-    
+
     @Test
     fun createAListVarargs() {
         val l = list(1, 2, 3, 4)
         assertThat(l.head).isEqualTo(1)
         assertThat(l.tail).isEqualTo(Cons(2, Cons(3, Cons(4, Nil))))
-        assertThat(l.tail.head).isEqualTo(2)        
     }
-    
+
     @Test
     fun headOfAnEmptyList() {
         assertThatThrownBy({
             Nil.head
         })
     }
-    
+
     @Test
-    fun breakDownToHeadAndTail() {
-        val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
-        val (h, t) = l
-        assertThat(h).isEqualTo(1)
-        assertThat(t).isEqualTo(Cons(2, Cons(3, Cons(4, Nil))))
+    fun dropElements() {
+        val l = list(4, 3, 2, 1)
+        assertThat(l.drop(2)).isEqualTo(list(2, 1))
     }
-    
+
     @Test
-    fun foldLeftSum() {
+    fun dropWhile() {
+        val l = list(1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+        assertThat(l.dropWhile({ e -> e < 20 })).isEqualTo(list(21, 34, 55, 89))
+    }
+
+    @Test
+    fun foldLeft() {
         val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
-        assertThat(l.foldLeft(0, {r, e -> r + e})).isEqualTo(10)
+        assertThat(l.foldLeft(0, { r, e -> r + e })).isEqualTo(10)
+        assertThat(l.foldLeft(1, { r, e -> r * e })).isEqualTo(24)
     }
 
     @Test
     fun foldRightSum() {
         val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
-        assertThat(l.foldRight(0, {e, r -> e + r})).isEqualTo(10)
+        assertThat(l.foldRight(0, { e, r -> e + r })).isEqualTo(10)
+        assertThat(l.foldRight(1, { e, r -> e * r })).isEqualTo(24)
+        assertThat(l.foldRightViaFoldLeft(0, { e, r -> e + r })).isEqualTo(10)
+        assertThat(l.foldRightViaFoldLeft(1, { e, r -> e * r })).isEqualTo(24)
+    }
+
+    @Test
+    fun reverseAList() {
+        val l = list("a", "b", "c")
+        assertThat(l.reverse()).isEqualTo(list("c", "b", "a"))
     }
     
     @Test
     fun appendTwoLists() {
-        val l1 = list(1, 2, 3)
-        val l2 = list(4, 5, 6)
-        
-        assertThat(l1.append(l2)).isEqualTo(list(1, 2, 3, 4, 5, 6))
-    }
-    
-    @Test
-    fun reverseAList() {
-        val l = list("a", "b", "c")
-        
-        assertThat(l.reverse()).isEqualTo(list("c", "b", "a"))
+        val l1: List<Number> = list(1, 2, 3)
+        val l2 = list(4.1, 5.1, 6.1)
+
+        assertThat(l1.append(l2)).isEqualTo(list(1, 2, 3, 4.1, 5.1, 6.1))
     }
 
     @Test
@@ -74,36 +82,29 @@ class ListTest {
     @Test
     fun sizeOfANonNilList() {
         val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
-
         assertThat(l.size()).isEqualTo(4)
     }
 
     @Test
     fun mapElements() {
         val l = Cons(1, Cons(2, Cons(3, Nil)))
-
         val l2 = l.map { e -> e.toString() }
-
         assertThat(l2).isEqualTo(Cons("1", Cons("2", Cons("3", Nil))))
     }
-    
+
     @Test
     fun testFlatten() {
         val l = list(list(1, 2), list(3, 4))
-        
         assertThat(List.flatten(l)).isEqualTo(list(1, 2, 3, 4))
-        
-        
     }
 
     @Test
     fun flatMapElements() {
         val l = Cons(1, Cons(2, Cons(3, Nil)))
-
-        val l2 = l.flatMap { e -> list(e, e) }
+        val l2 = l.flatMap { e -> list(e.toString(), e.toString()) }
 
         assertThat(l2)
                 .isEqualTo(
-                        Cons(1, Cons(1, Cons(2, Cons(2, Cons(3, Cons(3, Nil)))))))
+                        Cons("1", Cons("1", Cons("2", Cons("2", Cons("3", Cons("3", Nil)))))))
     }
 }
